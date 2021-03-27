9b2579c1d2b663444509e32705359547642f91e2
Ken Slack <kenslack95@gmail.com>
2021-03-27 14:45:38 -0500

    got icons

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ const pshowIcon = document.querySelector('.showPlayer i');
 
 const randomClass = ["hand-rock", "hand-paper", "hand-scissor"];
 const text = document.getElementById('condition');
+
+// const path = require('path');
+// const paper_icon = fs.readFileSync(path.join(__dirname, 'hand-paper.png'));
+// const rock_icon = fs.readFileSync(path.join(__dirname, 'hand-rock.png'));
+// const scissors_icon = fs.readFileSync(path.join(__dirname, 'hand-scissors.png'));
+
